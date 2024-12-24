f19cb3ae8e74225898f63e23f24758d5f5c5df06
Matt Herold <mattherold@icloud.com>
2024-12-24 11:46:23 +0100

    Added pagination.

--- app/controllers/admin/index.js
+++ app/controllers/admin/index.js
@@ -4,6 +4,108 @@ import { action } from '@ember/object';
 import { service } from '@ember/service';
 
 export default class AdminIndexController extends Controller {
+  queryParams = ['offset', 'limit'];
+
   @service router;
   @service backend;
+
+  @tracked totalRecords = null;
+
+  @tracked offset = 0;
+  @tracked limit = 10;
+
+  get offsetFormatted() {
+    return this.offset + 1;
+  }
+
+  get offsetNextFormatted() {
+    return Math.min(this.offset + this.limit, this.totalRecords);
+  }
+
+  get pageList() {
+    let showPages = 2;
+
+    let pageCount = Math.ceil(this.totalRecords / this.limit); // Zero based
+    let currentPage = this.offset / this.limit; // Zero based
+
+    let pageList = [];
+
+    if (currentPage > 0) {
+      pageList.push({
+        title: 1,
+        offset: 0,
+        isActive: false,
+      });
+    }
+
+    if (currentPage - showPages - 1 > 0) {
+      pageList.push({
+        title: '...',
+        offset: (currentPage - showPages - 1) * this.limit,
+        isActive: false,
+      });
+    }
+
+    for (var i = Math.max(currentPage - showPages, 1); i < currentPage; i++) {
+      pageList.push({
+        title: i + 1,
+        offset: i * this.limit,
+        isActive: false,
+      });
+    }
+
+    pageList.push({
+      title: currentPage + 1,
+      offset: this.offset,
+      isActive: true,
+    });
+
+    for (
+      var i = currentPage + 1;
+      i < Math.min(currentPage + 1 + showPages, pageCount - 1);
+      i++
+    ) {
+      pageList.push({
+        title: i + 1,
+        offset: i * this.limit,
+        isActive: false,
+      });
+    }
+
+    if (currentPage + 1 + showPages < pageCount - 1) {
+      pageList.push({
+        title: '...',
+        offset: (currentPage + 1 + showPages) * this.limit,
+        isActive: false,
+      });
+    }
+
+    if (currentPage < pageCount - 1) {
+      pageList.push({
+        title: pageCount,
+        offset: (pageCount - 1) * this.limit,
+        isActive: false,
+      });
+    }
+
+    return pageList;
+  }
+
+  @action setLimit(event) {
+    this.limit = event.target.value;
+    this.offset = 0;
+  }
+
+  get limitList() {
+    return [
+      {
+        limit: 10,
+        isActive: this.limit == 10,
+      },
+      {
+        limit: 20,
+        isActive: this.limit == 20,
+      },
+    ];
+  }
 }
--- app/routes/admin/index.js
+++ app/routes/admin/index.js
@@ -4,8 +4,31 @@ import { service } from '@ember/service';
 export default class AdminIndexRoute extends Route {
   @service store;
 
-  model() {
-    var model = this.store.findAll('contact');
+  queryParams = {
+    offset: {
+      refreshModel: true,
+    },
+    limit: {
+      refreshModel: true,
+    },
+  };
+
+  model(params) {
+    let offset = params.offset;
+    if (!offset) {
+      offset = 0;
+    }
+    let limit = params.limit;
+    if (!limit) {
+      limit = limit;
+    }
+    var model = this.store
+      .query('contact', { page: { offset: offset, limit: limit } })
+      .then((result) => {
+        let meta = result.meta;
+        this.controllerFor('admin/index').totalRecords = meta.total;
+        return result;
+      });
 
     return model;
   }
